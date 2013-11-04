9ec0f4c4c37485a18c1c2ba363aeb13163fb5183
Arnold Daniels <arnold@jasny.net>
2013-11-04 12:00:09 +0100

    Removed docs

--- docs/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Jasny\\DB\\Connection"],["c","Jasny\\DB\\Exception"],["c","Jasny\\DB\\ModelGenerator"],["c","Jasny\\DB\\MySQL\\Connection"],["c","Jasny\\DB\\MySQL\\Exception"],["c","Jasny\\DB\\MySQL\\Table"],["c","Jasny\\DB\\Record"],["c","Jasny\\DB\\Table"]];
