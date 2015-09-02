4daccb351adf81c2163b96cc657d9f5b686f2f68
Arnold Daniels <arnold@jasny.net>
2015-09-02 10:22:49 +0200

    Ability to specify group with Entity::hasUnique()

--- src/DB/Entity/UniqueProperties.php
+++ src/DB/Entity/UniqueProperties.php
@@ -10,8 +10,9 @@ interface UniqueProperties
     /**
      * Check no other entities with the same value of the property exists in the recordset
      * 
-     * @param string $property
+     * @param string       $property
+     * @param string|array $group     List of properties that should match
      * @return boolean
      */
-    public function hasUnique($property);
+    public function hasUnique($property, $group = null);
 }
