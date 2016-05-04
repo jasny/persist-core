d50cd2c884452de4981b3b31fb9951b9dc706aad
Arnold Daniels <arnold@jasny.net>
2016-05-04 19:10:13 +0200

    Added Entity\Redactable Moved `Enrichable::without()` and `withOnly()` to Redactable Redactable also has a `hasCensored($property)` method Smarter way to get only public properties for LazyLoading and Enrichable

---
+++ tests/Entity/Enrichable/ImplementationTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Jasny\DB\Entity\Enrichable;
+
+/**
+ * @covers \Jasny\DB\Entity\Enrichable\Implementation
+ */
+class ImplementationTest extends \PHPUnit_Framework_TestCase
+{
+    
+    
+    /**
+     * Run before each test case
+     */
+    public function setUp()
+    {
+        
+    }
+}
