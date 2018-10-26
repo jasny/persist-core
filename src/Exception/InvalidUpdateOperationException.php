d49d30a99ec68d9b6ab7971df927a7f3612c9aa4
Arnold Daniels <arnold@jasny.net>
2018-10-26 19:46:28 +0200

    Added `InvalidUpdateOperationException`

---
+++ src/Exception/InvalidUpdateOperationException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jasny\DB\Exception;
+
+/**
+ * Specified update operation is not valid.
+ */
+class InvalidUpdateOperationException extends \InvalidArgumentException
+{
+}
