94d423810fb366294bf66f851c98b30a277c2e67
Arnold Daniels <arnold@jasny.net>
2020-09-12 23:11:26 +0200

    Added Blackhole gateway

---
+++ src/Gateway/Blackhole.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Persist\Gateway;
+
+use Persist\Option\OptionInterface;
+use Persist\Result\Result;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Gateway without a storage. Data isn't saved.
+ *
+ * @implements GatewayInterface<null>
+ */
+final class Blackhole implements GatewayInterface
+{
+    /**
+     * Get underlying storage object.
+     *
+     * @return null
+     */
+    public function getStorage()
+    {
+        return null;
+    }
+
+    /**
+     * Does nothing.
+     *
+     * @param LoggerInterface $logger
+     * @return $this
+     */
+    public function withLogging(LoggerInterface $logger)
+    {
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function fetch(array $filter = [], OptionInterface ...$opts): Result
+    {
+        return new Result();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function count(array $filter = [], OptionInterface ...$opts): int
+    {
+        return 0;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function update(array $filter, $instruction, OptionInterface ...$opts): Result
+    {
+        return new Result();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function save($item, OptionInterface ...$opts): Result
+    {
+        return new Result([$item]);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function saveAll(iterable $items, OptionInterface ...$opts): Result
+    {
+        return new Result($items);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete(array $filter, OptionInterface ...$opts): Result
+    {
+        return new Result();
+    }
+}
